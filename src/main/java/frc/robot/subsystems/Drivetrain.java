f896bcac42033b30d550bece4950914dfbe1bdae
LunarKitties <lunarkitties5930@gmail.com>
2020-08-24 17:25:38 -0500

    Declared drive motors

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -11,12 +11,14 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj.controller.PIDController;
 
+import frc.robot.Constants;
+
 public class Drivetrain extends SubsystemBase{
-    CANSparkMax lfMotor = new CANSparkMax(ConfigMap.CAN_LF_DRIVE_MOTOR, MotorType.kBrushless);
-    CANSparkMax lmMotor = new CANSparkMax(ConfigMap.CAN_LM_DRIVE_MOTOR, MotorType.kBrushless);
-    CANSparkMax lbMotor = new CANSparkMax(ConfigMap.CAN_LB_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax lfMotor = new CANSparkMax(Constants.CAN_LF_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax lmMotor = new CANSparkMax(Constants.CAN_LM_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax lbMotor = new CANSparkMax(Constants.CAN_LB_DRIVE_MOTOR, MotorType.kBrushless);
     
-    CANSparkMax rfMotor = new CANSparkMax(ConfigMap.CAN_RF_DRIVE_MOTOR, MotorType.kBrushless);
-    CANSparkMax rmMotor = new CANSparkMax(ConfigMap.CAN_RM_DRIVE_MOTOR, MotorType.kBrushless);
-    CANSparkMax rbMotor = new CANSparkMax(ConfigMap.CAN_RB_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax rfMotor = new CANSparkMax(Constants.CAN_RF_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax rmMotor = new CANSparkMax(Constants.CAN_RM_DRIVE_MOTOR, MotorType.kBrushless);
+    CANSparkMax rbMotor = new CANSparkMax(Constants.CAN_RB_DRIVE_MOTOR, MotorType.kBrushless);
 }
