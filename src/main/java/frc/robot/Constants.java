5d86c4bd825c781fb13bb0e72efaa5f78db3c1c1
LunarKitties <lunarkitties5930@gmail.com>
2020-08-24 17:23:21 -0500

    Updated constants to contain Roborio port references

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,5 +16,51 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    
+    //NEO CAN IDs
+    //shooter
+  public static int CAN_L_SHOOTER_MOTOR = 8;
+  public static int CAN_R_SHOOTER_MOTOR = 9;
+    //drivetrain
+  public static int CAN_RF_DRIVE_MOTOR = 2;
+  public static int CAN_RM_DRIVE_MOTOR = 3;
+  public static int CAN_RB_DRIVE_MOTOR = 4;
+  public static int CAN_LF_DRIVE_MOTOR = 5;
+  public static int CAN_LM_DRIVE_MOTOR = 6;
+  public static int CAN_LB_DRIVE_MOTOR = 7;
+
+  //Talon CAN IDs
+    //indexor
+  public static int CAN_TALON_INDEX_MOTOR = 11;
+  public static int CAN_TALON_PRE_SHOOT_MOTOR = 10;
+    //turret
+  public static int CAN_TALON_TURRET_MOTOR = 13;
+    //accumulator
+  public static int CAN_TALON_INTAKE_MOTOR = 14;
+  public static int CAN_VICTOR_ADJUST_MOTOR = 16;
+    //lift
+  public static int CAN_VICTOR_LIFT_MOTOR = 12;  
+  public static int CAN_TALON_SKYWALKER_MOTOR = 15;
+
+  //PCM Ports
+  public static final int PCM_DRIVE_S_OUT = 2;
+  public static final int PCM_DRIVE_S_IN = 3;
+  public static final int PCM_BRAKE_OUT = 0;
+  public static final int PCM_BRAKE_IN = 1;
+
+  //PWM Ports
+  public static final int PWM_BLINKIN = 0;
+
+  //DIO Ports
+  public static final int DIO_TURRET_A_ENCODER = 0;
+  public static final int DIO_TURRET_B_ENCODER = 1;
+  public static final int DIO_INDEX_A_ENCODER = 2;
+  public static final int DIO_INDEX_B_ENCODER = 3;
+  public static final int DIO_ACCUM_A_ENCODER = 4;
+  public static final int DIO_ACCUM_B_ENCODER = 5;
+  public static final int DIO_INDEX_RIGHT_BALL_SWITCH = 7;
+  public static final int DIO_INDEX_LEFT_BALL_SWITCH = 8;
+  
+  //Analog Input Ports
+
+  //Global variables used
 }
