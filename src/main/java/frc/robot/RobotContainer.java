177a6bc4876f93d010b2acec79f8bc48de85abd0
lizprince04 <lizprince04@gmail.com>
2021-08-30 18:36:54 -0500

    Fixed errors in robot container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -31,6 +31,8 @@ import frc.robot.subsystems.Lift;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 
+import frc.robot.subsystems.LEDs;
+
 /**
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
  * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
@@ -41,6 +43,7 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private final Drivetrain mDrivetrain = new Drivetrain();
+  private final LEDs mLEDs = new LEDs();
   private final Lift mLift = new Lift();
   private final AccumulatorIntake mAccumulatorIntake = new AccumulatorIntake();
   private final AccumulatorJoint mAccumulatorJoint = new AccumulatorJoint();
@@ -97,8 +100,8 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
       //Shifting to low or high gear
-   new JoystickButton(xbox1, Button.kBumperRight.value).whenPressed(new WheelsShiftHigh(mDrivetrain));
-   new JoystickButton(xbox1, Button.kBumperLeft.value).whenPressed(new WheelsShiftLow(mDrivetrain));
+   new JoystickButton(xbox1, Button.kBumperRight.value).whenPressed(new WheelsShiftHigh(mDrivetrain, mLEDs));
+   new JoystickButton(xbox1, Button.kBumperLeft.value).whenPressed(new WheelsShiftLow(mDrivetrain, mLEDs));
       //Locking the lift in its current position
    new JoystickButton(xbox1, Button.kY.value).whenPressed(new LiftBrakeOn(mLift));
    new JoystickButton(xbox1, Button.kX.value).whenPressed(new LiftBrakeOff(mLift));
