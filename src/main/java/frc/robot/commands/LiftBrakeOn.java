eac8e6a20152baa1fec1f2b8f89c08108aaca805
lizprince04 <lizprince04@gmail.com>
2021-08-30 19:05:25 -0500

    Added LEDs to disc brake

--- src/main/java/frc/robot/commands/LiftBrakeOn.java
+++ src/main/java/frc/robot/commands/LiftBrakeOn.java
@@ -3,18 +3,22 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 import frc.robot.subsystems.Lift;
+import frc.robot.subsystems.LEDs;
 
 public class LiftBrakeOn extends CommandBase {
     private final Lift mLift;
+    private final LEDs mLEDs;
 
-    public LiftBrakeOn(Lift subsystem){
-        mLift = subsystem;
+    public LiftBrakeOn(Lift _Lift, LEDs _LEDs){
+        mLift = _Lift;
+        mLEDs = _LEDs;
         addRequirements(mLift);
     }
 
     @Override
     public void initialize(){
         mLift.brakeOn();
+        mLEDs.setColor(mLEDs.RED);
     }
 
     @Override
